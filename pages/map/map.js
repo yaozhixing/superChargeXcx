f64e88e361ac3a6b914ed5425954af03696af2e4
yaozhixing <yaozhixing797600@163.com>
2019-02-27 18:01:43 +0800

    精准定位。移动地图

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,33 +1,91 @@
+// 引入SDK核心类
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.min.js');
+
+// 实例化API核心类
+var qqmapsdk = new QQMapWX({
+  key: 'IV3BZ-O2WRD-75Q4W-HOIXA-FTOMK-2DFLQ' // 必填
+});
+
 Page({
-  /*data: { 
-    markers: [{ latitude: 23.099994, longitude: 113.324520, name: 'T.I.T 创意园', desc: '我现在的位置' }], 
-    covers: [
-      { latitude: 23.099794, longitude: 113.324520, icaonPath: '../images/car.png', rotate: 10 }, 
-      { latitude: 23.099298, longitude: 113.324129, iconPath: '../images/car.png', rotate: 90 }
-    ]
-  }*/
   data: {
-    myLocation:[]
+    //113.948837890625 22.555470648871527
+    longitude: 113.94883,
+    latitude: 22.55547,
+    scale: 15,   //缩放级别
+    //其他目的标记点
+    markers: [
+      {
+        id: 0,
+        title: "充电宝1号",
+        iconPath: "../../images/markers.png",
+        longitude: 113.94892,
+        latitude: 22.55559,
+        width: 35,
+        height: 38
+      },
+      {
+        id: 1,
+        title: "充电宝2号",
+        iconPath: "../../images/markers.png",
+        longitude: 113.94163,
+        latitude: 22.55127,
+        width: 35,
+        height: 38
+      },
+      {
+        id: 2,
+        title: "充电宝3号",
+        iconPath: "../../images/markers.png",
+        longitude: 113.94391,
+        latitude: 22.55147,
+        width: 35,
+        height: 38
+      },
+      {
+        id: 3,
+        title: "充电宝4号",
+        iconPath: "../../images/markers.png",
+        longitude: 113.94803,
+        latitude: 22.55247,
+        width: 35,
+        height: 38
+      },
+    ],
   },
 
   onReady(e){
-    this.mapCtx = wx.createMapContext('myMap');
-    this.getLocatPosition();
+    
   },
 
+  //拖动视野
+  bindregionchange(e){
+    //console.log(e)
+    //实时获取定位点的坐标
+    if (e.type == 'end' && (e.causedBy == 'scale' || e.causedBy == 'drag')){
+      type: 'gcj02',
+      this.mapCtx.getCenterLocation({
+        success(res){
+          console.log(res);
+        }
+      })
+    }
+  },
+  
+
   //获取位置
-  getCenterLocation(){
+  /*getCenterLocation(){
     var _this = this;
     this.mapCtx.getCenterLocation({
       success(res) {
         console.log(res);
         _this.setData({
-          myLocation: [res.latitude, res.longitude]
+          longitude: res.longitude,
+          latitude: res.latitude
         })
-        console.log(_this.data.myLocation);
+        console.log(_this.data.longitude, _this.data.latitude);
       }
     })
-  },
+  },*/
 
   /* 定位位置 */
   getLocatPosition(){
@@ -43,8 +101,24 @@ Page({
     })
   },
   
+  //加载
+  onLoad(){
+    var _this = this;
+    wx.getLocation({
+      type: 'gcj02',
+      success(res) {
+        _this.setData({
+          longitude: res.longitude,
+          latitude: res.latitude
+        })
+        console.log(_this.data.longitude, _this.data.latitude)
+      },
+    })
+  },
 
   onShow: function(){
-   
+    this.mapCtx = wx.createMapContext('myMap');
+    //this.getLocatPosition();
+    //this.getCenterLocation();
   }
 })
