076114e28525da18d52aea0635748297f9efbee4
yaozhixing <yaozhixing797600@163.com>
2019-03-02 15:01:33 +0800

    首页组件开发

---
+++ components/index-nav/index-nav.js
@@ -0,0 +1,107 @@
+// components/index-nav/index-nav.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navLists:[
+      {
+        title: '工薪贷',
+        img: '/assets/images/index/indnav-ico1.png',
+        url: ''
+      },
+      {
+        title: '房抵贷',
+        img: '/assets/images/index/indnav-ico2.png',
+        url: ''
+      },
+      {
+        title: '全国月供房',
+        img: '/assets/images/index/indnav-ico3.png',
+        url: ''
+      },
+      {
+        title: '经营贷',
+        img: '/assets/images/index/indnav-ico4.png',
+        url: ''
+      },
+      {
+        title: '公积金贷',
+        img: '/assets/images/index/indnav-ico5.png',
+        url: ''
+      },
+      {
+        title: '保单贷',
+        img: '/assets/images/index/indnav-ico6.png',
+        url: ''
+      },
+      {
+        title: '车抵贷',
+        img: '/assets/images/index/indnav-ico7.png',
+        url: ''
+      },
+      {
+        title: '车辆信用贷',
+        img: '/assets/images/index/indnav-ico8.png',
+        url: ''
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
